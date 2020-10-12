029fe0b2f16c3a154cc270eb949f21fdca8cf797
root <william.urban@navigaglobal.com>
2020-10-12 13:26:34 +0000

    version 2020-10-12T132617-v0.0.1

--- ingesterdoc.rb
+++ ingesterdoc.rb
@@ -5,8 +5,8 @@
 class IngesterLambdaGui < Formula
   desc "Ingester Lambda CLI"
   homepage "https://bitbucket.org/infomaker/"
-  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/ingester-doc/ingester-doc-2020-10-12T132559-adf84ca.tar.gz"
-  sha256 "b9237e05e04f546e8ec388becb17abe2e2df14df8bfe704aac995ee67bf5e84e"
+  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/ingester-doc/ingester-doc-2020-10-12T132617-v0.0.1.tar.gz"
+  sha256 "949138482cc3aa6363220efce9a9e76b26057b7ccef9cb60219659ce62b9abbe"
 
   def install
     bin.install "ingester-cli"
